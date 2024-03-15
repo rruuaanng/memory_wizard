f80c3f08bf62a096c683c542e25a69168c5780f4
RUANG <longjinyii@outlook.com>
2024-03-15 16:37:45 +0800

    将proc_desc移动到proc_scan.h

--- src/core.c
+++ src/core.c
@@ -11,17 +11,6 @@
 #include "include/wizard_user_interface.h"
 
 /*==========================
-global struct
-============================*/
-struct proc_desc {
-	pid_t pid;
-	char *name;
-	__u64 entry_addr;
-	__u64 end_addr;
-	struct proc_desc *parent;
-}target_proc;
-
-/*==========================
 wizard infomation
 ============================*/
 #define WIZARD_VERSION 				"v0.0.1"
