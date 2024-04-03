1c628b37542816f4cc8d8a3c3fc11c420a305749
RUANG <longjinyii@outlook.com>
2024-04-03 16:44:47 +0800

    Update wizard_user_interface.h

--- wizard_user_interface.h
+++ wizard_user_interface.h
@@ -5,7 +5,7 @@
 	ioctl command
 ============================*/
 #define GET_VERSION 				_IOR('W', 0x0001, char*)
-#define GET_PROC_ID					_IOR('W', 0x0002, pid_t)
+#define GET_PROC_ID				_IOR('W', 0x0002, pid_t)
 // #define GET_PROCESS				
 
 #endif // _MOON_WIZARD_USER_INTERFACE_H
